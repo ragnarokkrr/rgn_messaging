be30f76ca7520dfd58a613d71252b910a556396b
ragnarokkrr <ragnarokkrr@gmail.com>
2014-07-24 17:35:14 -0300

    java config pt2

--- rgn_messaging_pocs/rgn_messaging_pocs_reqrepl/src/main/java/org/rgn/messaging/poc/reqrepl/SpringModule.java
+++ rgn_messaging_pocs/rgn_messaging_pocs_reqrepl/src/main/java/org/rgn/messaging/poc/reqrepl/SpringModule.java
@@ -4,8 +4,8 @@ import java.util.Properties;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jndi.JndiTemplate;
-
 import org.springframework.jndi.JndiObjectFactoryBean;
 
 @Configuration
@@ -14,15 +14,7 @@ public class SpringModule {
 	@Bean
 	public JndiTemplate jndiTemplate() {
 
-		Properties jndiProps = new Properties();
-
-		jndiProps.setProperty("java.naming.factory.initial",
-				"org.apache.activemq.jndi.ActiveMQInitialContextFactory");
-		jndiProps.setProperty("java.naming.provider.url",
-				"tcp://localhost:61616");
-		jndiProps.setProperty("java.naming.security.principal", "system");
-		jndiProps.setProperty("java.naming.security.credentials", "manager");
-		jndiProps.setProperty("connectionFactoryNames", "QueueCF");
+		Properties jndiProps = defaultJndiProps();
 
 		JndiTemplate jndiTemplate = new JndiTemplate();
 
@@ -32,12 +24,35 @@ public class SpringModule {
 	}
 
 	@Bean
-	public JndiObjectFactoryBean jndiObjectFactoryBean() {
-		JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
-		
-		jndiObjectFactoryBean.setJndiTemplate(jndiTemplate());
+	public Properties defaultJndiProps() {
+		Properties jndiProps = new Properties();
 
+		jndiProps.setProperty("java.naming.factory.initial",
+				"org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+		jndiProps.setProperty("java.naming.provider.url",
+				"tcp://localhost:61616");
+		jndiProps.setProperty("java.naming.security.principal", "system");
+		jndiProps.setProperty("java.naming.security.credentials", "manager");
+		jndiProps.setProperty("connectionFactoryNames", "QueueCF");
+		return jndiProps;
+	}
+
+	@Bean
+	public JndiObjectFactoryBean jndiQueueConnectionFactory() {
+		JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+
+		jndiObjectFactoryBean.setJndiTemplate(jndiTemplate());
+		jndiObjectFactoryBean.setJndiName("QueueCF");
 		return jndiObjectFactoryBean;
+	}
+
+	@Bean
+	public CachingConnectionFactory cachingConnectionFactory() {
+		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
+		cachingConnectionFactory.setTargetConnectionFactory(jndiQueueConnectionFactory().getObject());
+		
+		return cachingConnectionFactory;
+
 	}
 
 }
